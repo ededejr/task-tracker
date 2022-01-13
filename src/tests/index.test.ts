f852d41c2c18e59c3310819e635627b8efb25d88
Edede Oiwoh <eoiwoh@gmail.com>
2022-01-13 12:35:18 -0500

    test: Write basic tests for TaskTracker

--- src/tests/index.test.ts
+++ src/tests/index.test.ts
@@ -1,7 +1,28 @@
-import Package from '..';
+import { performance } from 'perf_hooks';
+import TaskTracker from '../';
 
-describe('Package', () => {
-	test('Package runs', () => {
-		expect(Package('a')).toBe('a');
+jest.mock('perf_hooks', () => ({
+	performance: {
+		now: jest.fn()
+	}
+}));
+
+describe('Task', () => {
+	test('has start method', () => {
+		const tracker = new TaskTracker();
+		expect(typeof tracker.start).toBe('function');
+	});
+
+	test('has stop method', () => {
+		const tracker = new TaskTracker();
+		expect(typeof tracker.stop).toBe('function');
+	});
+
+	test('calls performance.now', () => {
+		const tracker = new TaskTracker();
+		const task = tracker.start();
+		expect(performance.now).toHaveBeenCalledTimes(1);
+		task.stop();
+		expect(performance.now).toHaveBeenCalledTimes(2);
 	});
 });
